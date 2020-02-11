20e8271bf441afa90f05e1ec3e5322973796070e
nreddy880 <niharikareddy32@gmail.com>
2020-02-11 23:32:15 +0530

    dry class added

---
+++ designprinciples/src/designprinciples/dry.java
@@ -0,0 +1,36 @@
+package designprinciples;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class main {
+	public main(String[] args) {
+		List<Girls> girls=new ArrayList<>();
+		girls.add(new Girls("Jessica","9745387292"));
+		System.out.println("Give me the Jessica number");
+		getGirl(girls,"Jessica");
+	}
+	private static void getGirl(List<Girls> girls,String girlName) {
+		List<Girls> girl=girls.stream().filter(row->girlName.equals(row.getName())).collect(Collectors.toList());
+		String phone=girl.get(0).getPhone();
+		System.out.println(phone);
+	}
+}
+class Girls{
+	private String name;
+	private String phone;
+	public Girls(String name,String phone) {
+		this.name=name;
+		this.phone=phone;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+
+}
+
