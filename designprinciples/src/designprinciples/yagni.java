ea975abcbbf77f93be9a75b625040bcf00f3d73c
nreddy880 <niharikareddy32@gmail.com>
2020-02-11 23:40:00 +0530

    added yagni

---
+++ designprinciples/src/designprinciples/yagni.java
@@ -0,0 +1,25 @@
+package designprinciples;
+
+import com.niharika.UserRepository;
+
+public class Main {
+	public static void main(String[] args) {
+		UserRepository userRepository=new UserRepository();
+		userRepository.getAll();
+	}
+}
+	class UserRepository{
+		public void getAll() {
+			System.out.println("returned all users");
+		}
+		/**private void getById() {
+		System.out.println("returned user by id");
+		}
+		private void getByName() {
+			System.out.println("returned user by name");
+		}
+		private void getByAge() {
+			System.out.println("returned user by age");
+		}**/
+	}
+
